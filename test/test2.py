f800fc498d14268d2cd1a700bd4c6a04f089ee1a
larry <1762861715@qq.com>
2024-01-12 23:01:33 +0800

    1

--- test/test2.py
+++ test/test2.py
@@ -1,11 +1,10 @@
 from typing import List, Union, TypeVar
 from abc import ABCMeta
 
-from sqlalchemy import false, true
 
 T = TypeVar('T', bound='Model')
 
-class Model(metaclass=ABCMeta):
+class LaModel(metaclass=ABCMeta):
 
     @classmethod
     def get(cls: type[T], id: Union[int, str]):
@@ -35,14 +34,12 @@ class FieldDescriptor:
 # 元类装饰器实现
 def table(_table_name:str):
     def wrapper(cls):
-        class DecoratedModel(Model, cls):
+        class DecoratedModel(LaModel, cls):
             tablename = _table_name  # 将表名存储到类属性中
             
             # 如果需要执行其他初始化操作，可以在 __init_subclass__ 中添加
             # @classmethod
-            # def __init_subclass__(cls, **kwargs):
-            #     super().__init_subclass__(**kwargs)
-            #     cls.tablename = name
+            # def __init_subclass__(cls, **kwargs):pass
 
         return DecoratedModel
     return wrapper
@@ -51,18 +48,23 @@ def table(_table_name:str):
 class User:
     id:str = FieldDescriptor(primary=True)
     name:str = FieldDescriptor()
-    @sql
-    def selectByAccountAndPassword(a:str,b:str):List[User]
+
+    # @sql
+    # def selectByAccountAndPassword(a:str,b:str):List[User]
 
     @sql
     def selectByAccountAndPassword(a:str,b:str):User
 
 
-# 现在 User 类已经有了 get 和 delete 方法
+# 1.现在 User 类已经有了 get 和 delete 方法，说明可以通过元类装饰器实现类的增强。添加我们想给类添加的常见
+# sql方法。元装饰器 @table('user')
 # User.get(1)
 # User.delete(1)
 
 
-User.selectByAccountAndPassword("123","123")
-    
-print(User.tablename,User.dictMap)
+#2. FieldDescriptor属性赋值方法，可以对字段进行收集，为后续的功能打下环境基础
+# print(User.tablename,User.dictMap)
+
+#.研究动态反射加方法
+# User.selectByAccountAndPassword("123","123")
+
