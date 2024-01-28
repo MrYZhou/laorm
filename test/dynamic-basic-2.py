fd8ab1a307195073041f0d0a5219a0945cfe3ecd
larry <1762861715@qq.com>
2024-01-29 00:43:35 +0800

    1

--- test/dynamic-basic-2.py
+++ test/dynamic-basic-2.py
@@ -1,28 +1,22 @@
 import re
 
-from stream import FieldDescriptor, table
+from stream import FieldDescriptor, SqlStateMachine, table
 
 
-
-
-@table('user')
+@table("user")
 class User:
-    id:str = FieldDescriptor(primary=True)
-    name:str = FieldDescriptor()
+    id: str = FieldDescriptor(primary=True)
+    name: str = FieldDescriptor()
 
 
-User.dynamic('selectByAccountAndPassword',[1,2])    
+state_machine = SqlStateMachine()
+state_machine.process_keyword("from", "user")
 
-def parseMethodToSql(dynamicSql: str):
-    # 使用正则表达式找到所有大写字母的位置并进行分割
-    parts = re.split('(?=[A-Z])', dynamicSql)
-    # 去除可能出现的空字符串（例如：首位是大写字母的情况）
-    parts = [part for part in parts if part]
-    return parts
 
 # 测试数据
 combined_input = "selectByAccountAndPassword"
-result = parseMethodToSql(combined_input)
-print(result)
+result = User.parseMethodToSql(combined_input)
+state_machine.finalize()
+print(state_machine.execute_sql)
 
-#parsql 完成后得到查询字符串然后只要直接用ppa执行exec即可
+# parsql 完成后得到查询字符串然后只要直接用ppa执行exec即可
