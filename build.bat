02e3d362eb827fc0c77a5b22b9f6099ca44b7604
larry <1762861715@qq.com>
2024-03-08 21:35:05 +0800

    构建脚本优化

--- build.bat
+++ build.bat
@@ -1,1 +1,12 @@
-poetry build --format=sdist && python -m twine upload dist/*
+@echo off
+
+REM 删除dist目录下的所有文件（不包括子目录）
+del /Q dist\*
+
+REM 如果上面命令执行成功，则继续构建和上传
+if %errorlevel% equ 0 (
+    poetry build --format=sdist
+    if %errorlevel% equ 0 (
+        python -m twine upload dist/*
+    )
+)
